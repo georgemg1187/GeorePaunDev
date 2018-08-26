8e92fc30e6e272d3df3b9a0586f81590e50f581d
George Paun <georgemg1187@gmail.com>
2018-08-26 18:49:58 +0300

    agust 26 up to abilities

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-var abilities = ['CSS', 'HTML', 'JSON', 'PHP', 'XML', 'JS', 'SEO', 'WP', 'SASS'];
+//Set Age starts here -----------------------
+var myBirthday = 'November 08, 1987 10:10:00';
+
+function calcAge(dateString) {
+    var birthday = +new Date(dateString);
+    return ~~((Date.now() - birthday) / (31557600000));
+  }
+
+var ageNode = document.getElementById('age');
+ageNode.textContent = calcAge(myBirthday);
+//Set Age ends here -------------------------
+
+
+var abilities = ['bootstrap', 'css', 'git', 'hithub', 'html', 'JSLogo', 'json', 'php', 'python', 'sass', 'seo', 'wordpress', 'xml'];
 var abilitiesRows = Array.prototype.slice.call(document.querySelectorAll('.ab-container'));
 
 function createRombs(element) {
@@ -11,10 +24,10 @@ function createRombs(element) {
     romb.appendChild(rombContent);
 }
 
-function grid(htmlNode, row, column) {
-    this.htmlNode = htmlNode;
-    this.row = htmlNode.innerWidth;
-    this.column = column;
-}
+// function grid(htmlNode, row, column)  {
+//     this.htmlNode = htmlNode;
+//     this.row = htmlNode.innerWidth;
+//     this.column = column;
+// }
 
 console.log(document.querySelector('#abilities .container-l').offsetWidth)
